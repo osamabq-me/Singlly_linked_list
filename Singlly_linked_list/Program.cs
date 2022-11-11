7dcb5ef35176201af8f35df8a4d7cd91969c8b02
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-11-11 17:14:44 +0700

    Creat the class node

--- Singlly_linked_list/Program.cs
+++ Singlly_linked_list/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Singlly_linked_list
 {
 
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
     class Program
     {
         static void Main(string[] args)
