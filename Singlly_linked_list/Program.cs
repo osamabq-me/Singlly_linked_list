1ef3da2a2ded421815185a60f16117247289dfcd
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-11-11 17:27:09 +0700

    Checking if the new roll number is duplicated

--- Singlly_linked_list/Program.cs
+++ Singlly_linked_list/Program.cs
@@ -33,6 +33,15 @@ namespace Singlly_linked_list
             Node newnode = new Node();
             newnode.rollNumber = nim;
             newnode.name = nm;
+            //if the node to be inserted is the first node 
+            if (START == null || nim <= START.rollNumber)
+            {
+                if ((START != null) && (nim == START.rollNumber))
+                {
+                    Console.WriteLine("\n Duplicate roll numbers not allowed\n");
+                    return;
+                }
+            }
         }
 
 
